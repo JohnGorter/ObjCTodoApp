575ba31dd1be9fbb7e4e77d3fe2753f942f74d7a
john gorter <john.gorter@gmail.com>
2015-07-13 11:28:33 +0200

    Initial Commit

---
+++ ObjCDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjCDemo
+//
+//  Created by John Gorter on 13-07-2015.
+//  Copyright (c) 2015 John Gorter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
